79a67105ce0231da3259676d2e5afbf6f58a0926
Crowdbotics <team@crowdbotics.com>
2023-11-14 13:26:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-leaf-44516",
+  url: "https://soft-leaf-44516.botics.co",
+  api: "https://soft-leaf-44516.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
